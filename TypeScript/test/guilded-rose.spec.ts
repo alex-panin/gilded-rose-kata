cab9ef09eb05bc624636f4670b54192de68f57ae
alex-panin <alexandervpanin@gmail.com>
2022-07-29 02:03:08 +0300

    add updater test

--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -1,12 +1,35 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { expect } from "chai";
+import { Item, GildedRose } from "../app/gilded-rose";
 
-describe('Gilded Rose', function () {
+describe("Gilded Rose", function () {
+  it("should correctly update once", function () {
+    const initialData = [
+      new Item("foo", 0, 0),
+      new Item("Aged Brie foo", 10, 10),
+      new Item("Backstage passes wow", 8, 10),
+      new Item("Conjured frog", 10, 40),
+      new Item("Conjured froggo", -2, 3),
+      new Item("Sulfuras frog", 8, 80),
+    ];
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+    const updatedDataMock = [
+      { name: "foo", sellIn: -1, quality: 0 },
+      { name: "Aged Brie foo", sellIn: 9, quality: 11 },
+      { name: "Backstage passes wow", sellIn: 7, quality: 12 },
+      { name: "Conjured frog", sellIn: 9, quality: 38 },
+      { name: "Conjured froggo", sellIn: -3, quality: 0 },
+      { name: "Sulfuras frog", sellIn: 8, quality: 80 },
+    ];
 
+    // just checking the dynamics;
+    // for (let i = 0; i < 20; i++) {
+    //   const gildedRose = new GildedRose(initialData);
+    //   const items = gildedRose.updateQuality();
+    //   console.log(items);
+    // }
+
+    const gildedRose = new GildedRose(initialData);
+    const items = gildedRose.updateQuality();
+    expect(items).to.deep.equal(updatedDataMock);
+  });
 });
